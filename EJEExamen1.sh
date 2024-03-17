ec8e9f77953fddc86a83a7dedd3b5f28bb2eb212
OlivierSanrey <138728729+OlivierSanrey@users.noreply.github.com>
2024-03-16 22:17:42 -0600

    ExamenPSA2.sh

--- EJEExamen1.sh
+++ EJEExamen1.sh
@@ -1,33 +1,35 @@
 #!/bin/bash
 
-# Verificar si se proporcionó un argumento
-if [ $# -eq 0 ]; then
-    echo "Error: Debes proporcionar la opción de imagen (1 o 2) como argumento."
-    exit 1
-fi
+# Función para mostrar el menú y obtener la opción seleccionada
+mostrar_menu() {
+    opcion=$(whiptail --title "Selección de Fondo de Pantalla" --menu "Elige una opción:" 15 50 3 \
+        "Imagen1" "Establecer el fondo de pantalla con la imagen 1" \
+        "Imagen2" "Establecer el fondo de pantalla con la imagen 2" \
+        "Salir" "Salir del programa" \
+        3>&1 1>&2 2>&3)
+    echo "$opcion"
+}
 
-# Obtener la opción de imagen del primer argumento
-opcion="$1"
+# Obtener la opción del menú
+opcion_seleccionada=$(mostrar_menu)
 
-# Verificar la opción de imagen y establecer la ruta de la imagen correspondiente
-case $opcion in
-    1)
+# Determinar la acción a realizar según la opción seleccionada
+case $opcion_seleccionada in
+    "Imagen1")
         imagen="/home/olivier/img1.jpg"
         ;;
-    2)
+    "Imagen2")
         imagen="/home/olivier/img2.jpg"
         ;;
+    "Salir")
+        echo "Saliendo del programa."
+        exit 0
+        ;;
     *)
-        echo "Error: Opción de imagen no válida. Debes elegir 1 o 2."
+        echo "Opción no válida."
         exit 1
         ;;
 esac
-
-# Verificar si la imagen existe en la ruta especificada
-if [ ! -f "$imagen" ]; then
-    echo "Error: La imagen no existe en la ruta especificada."
-    exit 1
-fi
 
 # Cambiar el fondo de pantalla utilizando gsettings
 gsettings set org.gnome.desktop.background picture-uri "file://$imagen"
